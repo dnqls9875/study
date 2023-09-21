acc4d4887dce46012eab4945fbab288a34f60f95
dnqls2446 <dnqls9875@gmail.com>
2023-09-21 23:07:02 +0900

    no message

---
+++ class/javascript/230906J/object/app.js
@@ -0,0 +1,22 @@
+const fitBitData = {
+  totalSteps: 308727,
+};
+
+// console.log(fitBitData);
+
+typeof fitBitData;
+
+console.log(typeof fitBitData);
+
+const person = {
+  firstName: "Mick",
+  lastName: "pardeon",
+};
+
+const kitchenSink = {
+  favNum: 92319023,
+  isFunny: true,
+  colors: ["red", "orange"],
+};
+
+kitchenSink.favNum = 4;
