46a6090370376c1620ec07c20b601cfbd0595476
[dnqls2446] <[dnqls9875@gmail.com]>
2023-09-25 17:56:20 +0900

    함수

---
+++ class/javascript/230922J/app.js
@@ -0,0 +1,11 @@
+function singSong() {
+  console.log("도");
+  console.log("레");
+  console.log("미");
+  console.log("파");
+  console.log("솔");
+  console.log("라");
+  console.log("시");
+  console.log("도");
+}
+singSong();
