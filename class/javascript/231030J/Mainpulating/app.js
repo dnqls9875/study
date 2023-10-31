d9c6b356d22b83642651dd6b06a83b7f07d6a3a1
[dnqls2446] <[dnqls9875@gmail.com]>
2023-10-31 17:46:40 +0900

    style

--- class/javascript/231030J/Mainpulating/app.js
+++ class/javascript/231030J/Mainpulating/app.js
@@ -1,7 +1,19 @@
-const allLinks = document.querySelectorAll("a");
+// const allLinks = document.querySelectorAll("a");
 
-for (let link of allLinks) {
-  link.innerText = "I am A Link!!!";
+// for (let link of allLinks) {
+//   link.innerText = "I am A Link!!!";
+// }
+
+const imgPick = document.querySelector("img");
+imgPick.src = "https://devsprouthosting.com/images/chicken.jpg";
+imgPick.alt = "chicken";
+
+console.log(imgPick);
+
+const links = document.querySelectorAll("a");
+
+for (let link of links) {
+  link.style.color = "green";
+  link.style.textDecorationColor = "magenta";
+  link.style.textDecorationStyle = "wavy";
 }
-
-document.querySelector("span").innerText = "Disgusting";
